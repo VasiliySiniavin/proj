4616f1ddc4a7b5dd454ba47eac2d07000771db9e
VasiliySiniavin <vasinyavin@yandex.ru>
2024-02-09 21:59:10 +0300

    Улучшена логика doSomething на распечатку

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -9,10 +9,6 @@ public class MainApplication {
     }
 
     public static void doSomething() {
-        System.out.println(1);
-        System.out.println(1);
-        System.out.println(1);
-        System.out.println(0);
-        System.out.println(1);
+        System.out.println("Hello World");
     }
 }
