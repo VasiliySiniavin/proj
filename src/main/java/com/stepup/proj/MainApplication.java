212b8028d24e13e772c1f1eb3cef13e2c076d18b
VasiliySiniavin <vasinyavin@yandex.ru>
2024-02-09 21:41:18 +0300

    Улучшили логику main и doSomething

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -4,12 +4,12 @@ public class MainApplication {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("1001");
-        System.out.println("_____");
+        System.out.println("------");
     }
 
     public static void doSomething() {
         System.out.println(1);
-        System.out.println(0);
-        System.out.println(0);
+        System.out.println(1);
+        System.out.println(1);
     }
 }
