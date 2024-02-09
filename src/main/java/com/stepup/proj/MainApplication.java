097e077927494f1077c9c089295dbe75ae487a8c
VasiliySiniavin <vasinyavin@yandex.ru>
2024-02-09 22:00:11 +0300

    Добавлен вызов doSomething

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -4,6 +4,7 @@ public class MainApplication {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("1001");
+        doSomething();
         System.out.println("_____----_");
         System.out.println("------");
     }
